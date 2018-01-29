7602ab59e40de13ad69e9c5a6641edffdf11547e
Vitalii Inshakov <michael8913@gmail.com>
2018-01-29 23:01:33 +0200

    implement auth and HOC

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,1 @@
-export const SAVE_COMMENT = 'SAVE_COMMENT';
+export const CHANGE_AUTH = 'CHANGE_AUTH';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import commentsReducer from './comments';
+import authReducer from './authentication';
 
 const rootReducer = combineReducers({
-  comments: commentsReducer
+  authenticated: authReducer
 });
 
 export default rootReducer;
