cd49a550285d44f963a519aae32a74c2903de14c
Vitalii Inshakov <michael8913@gmail.com>
2018-02-05 21:47:27 +0200

    fix secret code

--- server/router.js
+++ server/router.js
@@ -7,7 +7,7 @@ const requireSignin = passport.authenticate('local', { session: false });
 
 module.exports = function(app) {
   app.get('/', requireAuth, (req, res) => {
-    res.send({});
+    res.send({ message: 'Super secret code is 11111' });
   });
   app.post('/signin', requireSignin, Auth.signin);
   app.post('/signup', Auth.signup)
